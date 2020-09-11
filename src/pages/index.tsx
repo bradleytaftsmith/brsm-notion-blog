fd2aa7afb36b49848ae0bff9742493970db6d6be
brsm <bradleysmith.dev@gmail.com>
2020-09-11 12:15:25 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,6 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <img
-        src="/vercel-and-notion.png"
-        height="85"
-        width="250"
-        alt="Vercel + Notion"
-      />
       <h1>My Notion Blog</h1>
       <h2>
         Blazing Fast Notion Blog with Next.js'{' '}
