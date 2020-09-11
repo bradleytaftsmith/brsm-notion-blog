fe613e386f1063a4a6cabb8f9090e3425a5c24d5
brsm <bradleysmith.dev@gmail.com>
2020-09-11 12:25:08 -0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
+      <span>Deploy your own!</span>
         <ExtLink href="https://github.com/bradleytaftsmith/brsm-notion-blog">
           view source
         </ExtLink>
